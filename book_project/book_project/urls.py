428f63b31b38e96ae070d15768b68ddd715935bd
Masnekov Ivan <masnekovivan@gmail.com>
2021-01-13 16:33:56 +0300

    home page

--- book_project/book_project/urls.py
+++ book_project/book_project/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from book import views as book_views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', book_views.home, name='home')
+
 ]
